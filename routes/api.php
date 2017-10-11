0c4729ed89b969b7acacdea4326804c385953088
doldenburg <d.oldenburg@born4code.pl>
2017-10-11 19:23:23 +0200

    Refactor / add endpoints for api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,63 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users/{user}', [
+    'uses' => 'UserController@index'
+]);
+
+Route::post('/register', [
+    'uses' => 'UserController@store'
+]);
+
+Route::put('/users/{user}', [
+    'uses' => 'UserController@updatePassword'
+]);
+
+Route::delete('/users/{user}', [
+    'uses' => 'UserController@destroy'
+]);
+
+Route::post('/offers', [
+    'uses' => 'OfferController@store'
+]);
+
+Route::put('/offers/{offer}', [
+    'uses' => 'OfferController@update'
+]);
+
+Route::put('/offers/{offer}/status', [
+    'uses' => 'OfferController@updateStatus'
+]);
+
+Route::delete('/offers/{offer}', [
+    'uses' => 'OfferController@destroy'
+]);
+
+Route::get('/offers/{offer}', [
+    'uses' => 'OfferController@show'
+]);
+
+Route::get('/offers/user/{user}', [
+    'uses' => 'OfferController@showOffersByUser'
+]);
+
+Route::get('/offers/generateReport/{month}/{year}', [
+    'uses' => 'OfferController@generateReport'
+]);
+
+Route::post('/messages', [
+    'uses' => 'MessageController@store'
+]);
+
+Route::get('/messages/{user}', [
+    'uses' => 'MessageController@showMessagesByUser'
+]);
+
+Route::put('/messages/{message}', [
+    'uses' => 'MessageController@updateSeen'
+]);
+
+Route::delete('/messages/{message}', [
+    'uses' => 'MessageController@destroy'
+]);
