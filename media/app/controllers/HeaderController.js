9283d3bcf4bbed4cb77192ceb55619f23ce947b3
Josowsky <bartosz@jozwowiak.pl>
2017-10-14 10:39:23 +0200

    Add / show unseen messages

--- media/app/controllers/HeaderController.js
+++ media/app/controllers/HeaderController.js
@@ -5,7 +5,20 @@
     .module('application')
     .controller('HeaderController', HeaderController);
 
-  function HeaderController($scope, $rootScope) {
+  function HeaderController($scope, $rootScope, MessagesService) {
     $scope.usersVisible = $rootScope.currentUser.role === 'admin';
+    $scope.messages = [];
+    $scope.unseen = 0;
+
+    MessagesService.fetchAllMessages($rootScope.currentUser.id)
+      .then(function (response) {
+        $scope.messages = response.data.messages;
+        $scope.unseen = 0;
+        $scope.messages.forEach(function(message) {
+          if ( message.seen === 0 ) {
+            $scope.unseen += 1;
+          }
+        })
+      })
   }
 }());
